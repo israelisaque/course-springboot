a4a5a67ba1954044ef286a575e2528c981bb0b99
Israel Oliveira <israelisaque@hotmail.com>
2021-06-05 16:21:48 -0300

    Category

--- src/main/java/com/coursespring/course/entities/Order.java
+++ src/main/java/com/coursespring/course/entities/Order.java
@@ -81,9 +81,7 @@ public class Order implements Serializable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((client == null) ? 0 : client.hashCode());
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((moment == null) ? 0 : moment.hashCode());
 		return result;
 	}
 
@@ -96,20 +94,10 @@ public class Order implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Order other = (Order) obj;
-		if (client == null) {
-			if (other.client != null)
-				return false;
-		} else if (!client.equals(other.client))
-			return false;
 		if (id == null) {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (moment == null) {
-			if (other.moment != null)
-				return false;
-		} else if (!moment.equals(other.moment))
 			return false;
 		return true;
 	}
